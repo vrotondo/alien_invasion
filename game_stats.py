a8b1e69da381a201e03c77ffc070153ff17cb55b
vrotondo <150374510+vrotondo@users.noreply.github.com>
2024-03-19 10:08:44 -0400

    Add files via upload

---
+++ game_stats.py
@@ -0,0 +1,23 @@
+class GameStats:
+	# Track statistics for Alien Invasion.
+
+	def __init__(self, ai_game):
+		# Initializing statistics.
+		self.settings = ai_game.settings
+		self.reset_stats()
+
+		# Start game in an inactive state.
+		self.game_active = False
+
+		# Start Alien Invasion in an active state.
+		# self.game_active = True --> This one line of code caused me *so* much trouble!
+		# Nevertheless, let's keep it here just in case something breaks somewhere down the line.
+
+		# High score should never be reset.
+		self.high_score = 0
+
+	def reset_stats(self):
+		# Initialize statistics that can change during the game.
+		self.ships_left = self.settings.ship_limit
+		self.score = 0
+		self.level = 1
